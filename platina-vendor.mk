6738b07c36ad086ab4d6dddf2d86d21d8734d7dd
Andrey <32853251+andrey167@users.noreply.github.com>
2026-09-17 14:33:53 +0400

    bringup: wifi: build liblowi_wifihal from source
    The A13 prebuilt vendor blobs of liblowi_wifihal cause 'Unknown iface name: wlan0' - the vendor initialization doesn't find the wlan0 interface created by the built-in qca_cld3 driver. FP4 reference builds these from hardware/qcom-caf/wlan source. Remove the prebuilt copies from vendor.mk so the build uses the source-built A16-compatible version instead.

--- platina-vendor.mk
+++ platina-vendor.mk
@@ -939,7 +939,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/platina/proprietary/vendor/lib64/liblocationservice_glue.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblocationservice_glue.so \
     vendor/xiaomi/platina/proprietary/vendor/lib64/liblocationservice.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblocationservice.so \
     vendor/xiaomi/platina/proprietary/vendor/lib64/liblowi_client.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblowi_client.so \
-    vendor/xiaomi/platina/proprietary/vendor/lib64/liblowi_wifihal.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblowi_wifihal.so \
     vendor/xiaomi/platina/proprietary/vendor/lib64/liblqe.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblqe.so \
     vendor/xiaomi/platina/proprietary/vendor/lib64/libmdmdetect.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmdmdetect.so \
     vendor/xiaomi/platina/proprietary/vendor/lib64/libmdsprpc.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmdsprpc.so \
